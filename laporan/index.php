49af4f97cc90201431d14f200eb202f860284028
mrdennychandra <dennyc.dev@gmail.com>
2017-10-08 15:42:33 +0700

    sample filter hari

--- laporan/index.php
+++ laporan/index.php
@@ -5,7 +5,30 @@ include '../fragment/header.php';
 include '../fragment/menu.php';
 $con = connect_db();
 ?>
+<script type="text/javascript">
+    $(function () {
+        $('input.datepicker').each(function () {
+            var datepicker = $(this);
+            datepicker.bootstrapDatePicker(datepicker.data());
+        });
+    });
+</script>
 <main>
+    <form method="post" class="form-horizontal">
+        <div class="form-group">
+            <label class="control-label col-sm-4" for="tgllahir">Tanggal:</label>
+            <div class="col-sm-6">
+                <input type="text" name="tanggal" id="tanggal" 
+                       class="form-control datepicker" 
+                       data-date-format="DD-MM-YYYY"
+                       required="required"/>
+            </div>
+            <div class="col-sm-2">
+                <input type="submit" class="btn btn-success" value="Cari" id="submit" name="submit">
+            </div>
+        </div>
+
+    </form>
     <div class="row" id="divcetak">
         <h2>Laporan Penjualan</h2>
         <table class="table table-striped">
@@ -15,10 +38,22 @@ $con = connect_db();
                 <th>Penjualan</th>
             </tr>
             <?php
+            $where = "";
+            $filter = "";
+            if(isset($_POST['submit'])){
+                if(!empty($_POST['tanggal'])){
+                    $tanggal = $_POST['tanggal'];
+                    $filter = " tanggal = '$tanggal'";
+                }
+            }
+            if($filter != ""){
+                $where = " WHERE ";
+            }
+           
             $query = "SELECT DATE_FORMAT(tanggal,'%d-%m-%Y') AS tanggal, "
                     . "subtotal,m.nama FROM pesanan p "
                     . "INNER JOIN makanan m ON m.makananid=p.makananid "//ingat spasi disini
-                    . "INNER JOIN nota n ON n.notaid = p.notaid "; //ingat spasi disini
+                    . "INNER JOIN nota n ON n.notaid = p.notaid $where $filter"; //ingat spasi disini
             $result = execute_query($con, $query);
             $total = 0;
             while ($data = mysqli_fetch_array($result)) {
